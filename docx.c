b1028339712a95a5b4e7c17b19ba16378bb408e8
krish-2010 <109260441+krish-2010@users.noreply.github.com>
2022-07-14 15:31:06 +0530

    Update docx.c

--- docx.c
+++ docx.c
@@ -4,4 +4,10 @@ int main(){
 	for(int i=0;i<10;i++){
 		printf("%d Hello\n",i);
 	}
+	int n=7;
+	int* arr=(int*)malloc(n*sizeof(int));
+	printf("Printing array created using malloc in C\n");
+	for(int i=0;i<7;i++){
+		printf("%d ",*(arr+i));
+	}
 }
